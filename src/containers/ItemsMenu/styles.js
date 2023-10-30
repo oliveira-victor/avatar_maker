e4ec08a7e483863c195e376eaace05d90bb3c85a
victor <victor.freire.oliveira@gmail.com>
2023-10-30 11:06:01 -0300

    customizing scroll bar, fix: unwanted  scroll y on thumbnails

--- src/containers/ItemsMenu/styles.js
+++ src/containers/ItemsMenu/styles.js
@@ -8,6 +8,25 @@ export const ItemsContainer = styled.div`
         display: flex;
         overflow-x: scroll;
     }
+
+    @media (min-width: 1024px) {
+        &::-webkit-scrollbar-track {
+            -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+            border-radius: 10px;
+            background-color: #F5F5F5;
+        }
+
+        &::-webkit-scrollbar {
+            width: 12px;
+            background-color: #F5F5F5;
+        }
+
+        &::-webkit-scrollbar-thumb {
+            border-radius: 10px;
+            -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+            background-color: #c9c9c9;
+        }
+    }
 `
 
 export const Card = styled.div`
@@ -34,7 +53,6 @@ export const Items = styled.div`
     @media (max-width: 767px) {
         flex-wrap: nowrap;
         overflow-x: scroll;
-        padding-top: 20px;
         margin-top: 0;
     }
 
