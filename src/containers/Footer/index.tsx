d4ed7fac44bf7a68997ac5d1c001c9e01987fa28
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-02-19 14:20:30 -0300

    feat: footer finished

--- src/containers/Footer/index.tsx
+++ src/containers/Footer/index.tsx
@@ -1,9 +1,29 @@
 import * as S from './styles'
 
+import vfoStudio from '../../assets/images/vfostudio.svg'
+import vfo from '../../assets/images/vfo_white.svg'
+import github from '../../assets/images/github.svg'
+
 const Footer = () => {
     return (
         <S.Footer>
-            <h2>footer</h2>
+            <S.FooterContent>
+                <div>
+                    <h2>Avatar Maker</h2>
+                    <span>by Victor Freire Oliveira</span> <span>- Software Engineer</span> | <span>&copy; {new Date().getFullYear()}</span>
+                </div>
+                <S.footerLogos>
+                    <a href="https://victoroliveira.vercel.app/" target='_blank'>
+                        <img src={vfo} alt="VFO logo" title="Go to Victor's portfolio" />
+                    </a>
+                    <a href="https://github.com/oliveira-victor" target='_blank'>
+                        <img src={github} alt="GitHub logo" title="Go to Victor's GitHub page" />
+                    </a>
+                    <a href="https://vfostudio.vercel.app/" target='_blank'>
+                        <img src={vfoStudio} alt="Logo VFO Studio" title='Go to VFO Studio page' />
+                    </a>
+                </S.footerLogos>
+            </S.FooterContent>
         </S.Footer>
     )
 }
--- src/containers/Footer/styles.ts
+++ src/containers/Footer/styles.ts
@@ -1,7 +1,46 @@
 import styled from "styled-components";
 import { colors } from "../../styles/colors";
+import { screenSize } from "../../styles/screenSize";
 
 export const Footer = styled.footer`
     background-color: ${colors.darkBg};
     color: #fff;
+    padding: 40px;
+`
+
+export const FooterContent = styled.div`
+    max-width: 1440px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    
+    @media (max-width: ${screenSize.phone}) {
+        flex-wrap: wrap;
+        text-align: center;
+        justify-content: center;
+        gap: 40px;
+    }
+
+    h2 {
+        font-size: 30px;
+        font-family: 'Ubuntu', sans-serif;
+    }
+
+    span {
+        white-space: nowrap;
+    }
+`
+
+export const footerLogos = styled.div`
+    display: flex;
+    gap: 20px;
+
+    img {
+        height: 40px;
+        transition: all .2s ease;
+
+        &:hover {
+            transform: translateY(-4px);
+        }
+    }
 `
